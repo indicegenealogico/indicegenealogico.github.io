05d6da889cc966454704111f3df0feb92ddc2355
Indice Genealogico <indicegenealogico@gmail.com>
2022-01-10 21:08:54 -0700

    1901-1902
    AGregados anos de carupano

--- js/lastupdate.js
+++ js/lastupdate.js
@@ -1,4 +1,4 @@
   //Cambiar fecha de actualizacion
-  var up_date = "Enero 3, 2022";
+  var up_date = "Enero 10, 2022";
   var fecha = document.getElementById('update');
   fecha.innerHTML = up_date;

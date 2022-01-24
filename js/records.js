525b6d299f6d2fc78d1937ee397714a3452334de
Indice Genealogico <indicegenealogico@gmail.com>
2022-01-23 21:51:22 -0700

    update date

--- js/records.js
+++ js/records.js
@@ -113,3 +113,25 @@ var projects = [
   '<tr><td>     </td><td>        </td><td>Bautizos</td><td>1920-1925</td></tr>',
   '<tr><td>     </td><td>Guiria  </td><td>Bautizos</td><td>1926-1929</td></tr>',
 ];
+
+
+
+  //Cambiar fecha de actualizacion
+  var up_date = "Enero 23, 2022";
+  var fecha = document.getElementById('update');
+  fecha.innerHTML = up_date;
+
+
+
+  var updates = [
+  '<li class="list-group-item"><small>01/23/2022 | Finalizado - <a href="https://www.familysearch.org/records/images/image-details?page=2&place=2423652&rmsId=M9SM-SJZ&imageIndex=0&singleView=true">Tomo 12</a> de <a href="https://www.familysearch.org/wiki/es/G%C3%BCiria,_Sucre,_Venezuela_-_Genealog%C3%ADa">Guiria</a> - Bautizos 1920-1926 Inmaculada Concepción.</small></li>',
+  '<li class="list-group-item"><small>01/12/2022 | Finalizado - <a href="https://www.familysearch.org/records/images/image-details?page=2&place=2423652&rmsId=M9SM-SJC&imageIndex=0&singleView=true">Tomo 11</a> de <a href="https://www.familysearch.org/wiki/es/G%C3%BCiria,_Sucre,_Venezuela_-_Genealog%C3%ADa">Guiria</a> - Bautizos 1917-1920 Inmaculada Concepción.</small></li>',
+  '<li class="list-group-item"><small>01/11/2022 | Agregados - Bautizos 1902-1904 Santa Rosa - Carupano.</small></li>',
+  '<li class="list-group-item"><small>01/03/2021 | Finalizado <a href="https://www.familysearch.org/records/images/image-details?page=2&place=2423652&rmsId=M9SM-SJH&imageIndex=0&singleView=true">Tomo 10</a> de <a href="https://www.familysearch.org/wiki/es/G%C3%BCiria,_Sucre,_Venezuela_-_Genealog%C3%ADa">Guiria</a> - Bautizos 1912-1914.</small></li>',
+  '<li class="list-group-item"><small>01/01/2022 | Agregada página con Lista de <a href="./apellidos.html">Apellidos</a> en Índice Genealógico.</small></li>',
+  '<li class="list-group-item"><small>12/25/2021 | Finalizado <a href="https://www.familysearch.org/records/images/image-details?page=2&place=2423652&rmsId=M9SM-SJ4&imageIndex=0&singleView=true">Tomo 9</a> de <a href="https://www.familysearch.org/wiki/es/G%C3%BCiria,_Sucre,_Venezuela_-_Genealog%C3%ADa">Guiria</a> - Bautizos 1907-1912.</small></li>',
+  '<li class="list-group-item"><small>12/24/2021 | Primeros 10.000 índices cambiados de imágenes a registros.</small></li>',
+  '<li class="list-group-item"><small>12/13/2021 | Alcanzados los 50 mil índices.</small></li>',
+  '<li class="list-group-item"><small>01/28/2021 | Se abre una página de <a href="https://www.facebook.com/IndiceGenealogico/">Facebook</a> para Índice Genealólico.</small></li>',
+  '<li class="list-group-item"><small>10/25/2020 | Se da inicio al proyecto de Índice Genealógico.</small></li>', 
+];

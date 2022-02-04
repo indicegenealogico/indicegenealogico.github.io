f5d661c20f15b6fe91ebb9f7550a48ed3310b2e7
Indice Genealogico <indicegenealogico@gmail.com>
2022-02-03 20:54:42 -0700

    Tomo13 Guiria

--- js/records.js
+++ js/records.js
@@ -55,6 +55,7 @@ var recordset = [
   { id:'58', locat: '2423652', type: 'Bautizo', rmsID: 'M9SM-SJH', title: 'Guiria - Inmaculada Concepción 1912-1917', size: 0 },
   { id:'59', locat: '2423652', type: 'Bautizo', rmsID: 'M9SM-SJC', title: 'Guiria - Inmaculada Concepción 1917-1920', size: 100 },
   { id:'60', locat: '2423652', type: 'Bautizo', rmsID: 'M9SM-SJZ', title: 'Guiria - Inmaculada Concepción 1920-1926', size: 203 },
+  { id:'61', locat: '2423652', type: 'Bautizo', rmsID: 'M9SM-SJ8', title: 'Guiria - Inmaculada Concepción 1926-1929', size: 212 },
 ];
 
 var localidades = [ 
@@ -111,19 +112,20 @@ var localidades = [
 var projects = [
   '<tr><td>Sucre</td><td>Carúpano</td><td>Bautizos</td><td>1899-1907</td></tr>',
   '<tr><td>     </td><td>        </td><td>Bautizos</td><td>1920-1925</td></tr>',
-  '<tr><td>     </td><td>Guiria  </td><td>Bautizos</td><td>1926-1929</td></tr>',
+  '<tr><td>     </td><td>Guiria  </td><td>Bautizos</td><td>1929-1936</td></tr>',
 ];
 
 
 
   //Cambiar fecha de actualizacion
-  var up_date = "Enero 24, 2022";
+  var up_date = "Febrero 3, 2022";
   var fecha = document.getElementById('update');
   fecha.innerHTML = up_date;
 
 
 
   var updates = [
+  '<li class="list-group-item"><small>02/03/2022 | Finalizado - <a href="https://www.familysearch.org/records/images/image-details?page=2&place=2423652&rmsId=M9SM-SJ8&imageIndex=0&singleView=true">Tomo 12</a> de <a href="https://www.familysearch.org/wiki/es/G%C3%BCiria,_Sucre,_Venezuela_-_Genealog%C3%ADa">Guiria</a> - Bautizos 1926-1929 Inmaculada Concepción.</small></li>',
   '<li class="list-group-item"><small>01/24/2022 | Agregada página con Lista de <a href="../nombres/">Nombres</a> en Índice Genealógico.</small></li>',
   '<li class="list-group-item"><small>01/23/2022 | Finalizado - <a href="https://www.familysearch.org/records/images/image-details?page=2&place=2423652&rmsId=M9SM-SJZ&imageIndex=0&singleView=true">Tomo 12</a> de <a href="https://www.familysearch.org/wiki/es/G%C3%BCiria,_Sucre,_Venezuela_-_Genealog%C3%ADa">Guiria</a> - Bautizos 1920-1926 Inmaculada Concepción.</small></li>',
   '<li class="list-group-item"><small>01/12/2022 | Finalizado - <a href="https://www.familysearch.org/records/images/image-details?page=2&place=2423652&rmsId=M9SM-SJC&imageIndex=0&singleView=true">Tomo 11</a> de <a href="https://www.familysearch.org/wiki/es/G%C3%BCiria,_Sucre,_Venezuela_-_Genealog%C3%ADa">Guiria</a> - Bautizos 1917-1920 Inmaculada Concepción.</small></li>',

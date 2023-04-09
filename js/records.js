a8ac9068abbcba89f44649850594906efef926f7
Indice Genealogico <indicegenealogico@gmail.com>
2023-04-09 15:22:17 -0600

    Update records.js

--- js/records.js
+++ js/records.js
@@ -171,7 +171,7 @@ var localidades = [
 
 
 // Cambiar fecha de actualizacion
-document.getElementById('update').innerHTML = "Abr 8, 2023";
+document.getElementById('update').innerHTML = "Abr 9, 2023";
 
 // Extrae el año actual y lo agrega al copyright
 const fechaActual = new Date();
@@ -180,6 +180,7 @@ document.getElementById("copyright").textContent = "© 2020 - " + actualYear;
 
 
 var updates = [
+  '<li class="list-group-item"><small>04/09/2023 | Se agrega libro de Paleografia a la Nueva Biblioteca de Libros.</small></li>',
   '<li class="list-group-item"><small>04/08/2023 | Agregado Libro Historico de Apellidos de Valencia 1972.</small></li>',
   '<li class="list-group-item"><small>04/05/2023 | Agregado hijos de Matrimonios de Tunapuy 1894-1999 San Juan Bautista.</small></li>',
   '<li class="list-group-item"><small>03/14/2023 | Agregado Tomo # de Soro - Bautizos 1899-1904 San Jose.</small></li>',
